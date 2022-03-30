08d60279267e2402de8d2daf739d1ba6482c9a09
OtonielBN <000otoniel000@gmail.com>
2022-03-30 10:50:39 -0600

    cambio de ventana
    se añadió un cambio de ventana a la hora de hacer el JSON

--- ide-triangle-v1.1.src/src/GUI/Main.java
+++ ide-triangle-v1.1.src/src/GUI/Main.java
@@ -739,6 +739,7 @@ public class Main extends javax.swing.JFrame {
     private void compileFileMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_compileFileMenuItemActionPerformed
         // TODO add your handling code here:
         output.setDelegate(delegateConsole);
+        ((FileFrame) desktopPane.getSelectedFrame()).selectConsole();
         Jsongenerator.main(new String[] {desktopPane.getSelectedFrame().getTitle().replace(".tri", ".tam")});
     }//GEN-LAST:event_compileFileMenuItemActionPerformed
 
